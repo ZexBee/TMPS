b3d5871d9ea377e99f1de41dec2c3bfa6d54f7e8
Cristian Cebotari <cristian.cebotari@isa.utm.md>
2021-03-10 23:28:13 +0200

    Singleton1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            logginUser user1 = logginUser.Initialise();
+            logginUser user2 = logginUser.Initialise();
+            
+           /* ... */
+
+            Console.ReadKey();
+            
+        }
+    }
+}
