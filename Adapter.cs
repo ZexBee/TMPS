ec68fa81a7875f9a878de62408b9781b39d5139d
Cebotari Cristian <71315869+ZexBee@users.noreply.github.com>
2021-06-08 02:51:12 +0300

    Add files via upload

---
+++ Adapter.cs
@@ -0,0 +1,66 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Driver driver = new Driver();
+      
+        Auto auto = new Auto();
+        
+        driver.Travel(auto);
+       
+        Camel camel = new Camel();
+       
+        ITransport camelTransport = new CamelToTransportAdapter(camel);
+       
+        driver.Travel(camelTransport);
+ 
+        Console.Read();
+    }
+}
+interface ITransport
+{
+    void Drive();
+}
+
+class Auto : ITransport
+{
+    public void Drive()
+    {
+        Console.WriteLine("The car is driven on the road");
+    }
+}
+class Driver
+{
+    public void Travel(ITransport transport)
+    {
+        transport.Drive();
+    }
+}
+
+interface IAnimal
+{
+    void Move();
+}
+
+class Camel : IAnimal
+{
+    public void Move()
+    {
+        Console.WriteLine("The camel walks on sand");
+    }
+}
+
+class CamelToTransportAdapter : ITransport
+{
+    Camel camel;
+    public CamelToTransportAdapter(Camel c)
+    {
+        camel = c;
+    }
+ 
+    public void Drive()
+    {
+        camel.Move();
+    }
+}
